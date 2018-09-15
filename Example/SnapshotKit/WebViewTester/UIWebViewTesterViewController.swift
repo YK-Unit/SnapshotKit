db9293f58345f53844e1131fa76d4c76c9a16e87
York <yorkzhang520@gmail.com>
2018-09-15 16:34:59 +0800

    【SnapshotKit-Demo】更新UIWebView截图演示Demo

--- Example/SnapshotKit/WebViewTester/UIWebViewTesterViewController.swift
+++ Example/SnapshotKit/WebViewTester/UIWebViewTesterViewController.swift
@@ -12,6 +12,15 @@ import SVProgressHUD
 
 class UIWebViewTesterViewController: UIViewController {
 
+    private let tableView = UITableView()
+    private let cellReuseIdentifier = "CellReuseIdentifier"
+    private let configDatas: [String] = [
+        "截图可视区域",
+        "同步截图全部区域",
+        "异步截图全部区域_分页截图方式",
+        "异步截图全部区域_打印PDF方式"
+    ]
+
     private let webView = UIWebView()
 
     override func viewDidLoad() {
@@ -29,14 +38,15 @@ class UIWebViewTesterViewController: UIViewController {
 
         self.view.backgroundColor = UIColor.white
 
-        let items = ["截图可视区域", "同步截图全部区域", "异步截图全部区域"]
-        let segmentedControl = UISegmentedControl.init(items: items)
-        segmentedControl.isMomentary = true
-        segmentedControl.addTarget(self, action: #selector(onSegmentValueChange(_:)), for: .valueChanged)
-        segmentedControl.frame = CGRect.init(x: 5, y: 100, width: self.view.bounds.width - 10, height: 40)
-        self.view.addSubview(segmentedControl)
+        let tableViewHeight: CGFloat = CGFloat(configDatas.count * 30)
+        tableView.frame = CGRect.init(x: 0, y: 100, width: self.view.bounds.width, height: tableViewHeight)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
+        self.view.addSubview(tableView)
 
-        webView.frame = CGRect.init(x: 0, y: 200, width: self.view.frame.size.width, height: self.view.frame.size.height - 200)
+        let webView_Y: CGFloat = tableView.frame.origin.y +  tableView.frame.size.height + 20
+        webView.frame = CGRect.init(x: 0, y: webView_Y, width: self.view.frame.size.width, height: self.view.frame.size.height - webView_Y)
         webView.backgroundColor = UIColor.lightGray
         self.view.addSubview(webView)
 
@@ -46,20 +56,6 @@ class UIWebViewTesterViewController: UIViewController {
         //let urlStr = "https://www.jianshu.com/p/d238a844a1e6"
 
         webView.loadRequest(URLRequest(url: URL(string: urlStr)!))
-    }
-
-    @objc
-    private func onSegmentValueChange(_ segmentedControl: UISegmentedControl) {
-        switch segmentedControl.selectedSegmentIndex {
-        case 0:
-            takeSnapshotOfVisibleContent()
-        case 1:
-            sync_takeSnapshotOfFullContent()
-        case 2:
-            async_takeSnapshotOfFullContent()
-        default:
-            break
-        }
     }
 
     private func takeSnapshotOfVisibleContent() {
@@ -72,11 +68,66 @@ class UIWebViewTesterViewController: UIViewController {
         ShotImagePreviewInterface.previewShotImage(image)
     }
 
-    private func async_takeSnapshotOfFullContent() {
+    private func async_takeSnapshotOfFullContent_bySpliter() {
+        SVProgressHUD.show()
+        self.webView.scrollView.takeScreenshotOfFullContent { (image) in
+            SVProgressHUD.dismiss()
+            ShotImagePreviewInterface.previewShotImage(image)
+        }
+    }
+
+    private func async_takeSnapshotOfFullContent_byPrinter() {
         SVProgressHUD.show()
         self.webView.takeScreenshotOfFullContent { (image) in
             SVProgressHUD.dismiss()
             ShotImagePreviewInterface.previewShotImage(image)
+        }
+    }
+
+}
+
+// MARK: - UITableViewDataSource & UITableViewDelegate
+extension UIWebViewTesterViewController: UITableViewDataSource, UITableViewDelegate {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let num = configDatas.count
+
+        return num
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier, for: indexPath)
+
+        let row = indexPath.row
+        let cellName = configDatas[row]
+        cell.textLabel?.text = cellName
+
+        return cell
+    }
+
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 30
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let row = indexPath.row
+
+        switch row {
+        case 0:
+            takeSnapshotOfVisibleContent()
+        case 1:
+            sync_takeSnapshotOfFullContent()
+        case 2:
+            async_takeSnapshotOfFullContent_bySpliter()
+        case 3:
+            async_takeSnapshotOfFullContent_byPrinter()
+        default:
+            break
         }
     }
 }
